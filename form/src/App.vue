6dbd890aa7f1d1caba5c127b8d88d07614443c94
setiawanswan <setiawan.tumilin@gmail.com>
2021-08-27 15:19:31 +0700

    create simple form

--- form/src/App.vue
+++ form/src/App.vue
@@ -1,8 +1,66 @@
 <template>
   <div>
-    <app-header></app-header>
+    <app-header/>
     <div class="container">
+      <form>
+        <div class="row">
+          <div class="col-xs-12">
+            <h1>Contact Us</h1>
+            <hr>
 
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input 
+                type="text" 
+                id="name" 
+                class="form-control"
+                v-model.lazy="formdata.name"
+              >
+              {{ formdata.name }}
+            </div>
+            
+             <div class="form-group">
+              <label for="lastname">Lastname</label>
+              <input 
+                type="text" 
+                id="lastname" 
+                class="form-control"
+                v-model="formdata.lastname"
+              >
+            </div>
+
+             <div class="form-group">
+              <label for="subject">Subject</label>
+              <input 
+                type="text" 
+                id="subject" 
+                class="form-control"
+                v-model="formdata.subject"
+              >
+            </div>
+
+            <div class="form-group">
+              <label for="message">Message</label>
+              <textarea
+                class="form-control"
+                rows="3"
+                id="message"
+                v-model="formdata.message"
+              >
+                Hello
+              </textarea>
+            </div>
+
+            <button
+              class="btn btn-primary"
+              @click.prevent="submitForm"
+            >
+              Submit
+            </button>
+
+          </div>
+        </div>
+      </form>
     </div>
     <Footer />
   </div>
@@ -15,6 +73,22 @@ export default {
   name: 'App',
   components: {
     Footer
+  },
+  data(){
+    return {
+      formdata:{
+        name:'',
+        lastname:'', 
+        subject:'',
+        message:'Some default content'
+      }
+    }
+  },
+  methods:{
+    submitForm(){
+      // console.log(this.formdata)
+      console.log(JSON.stringify(this.formdata))
+    }
   }
 }
 </script>
